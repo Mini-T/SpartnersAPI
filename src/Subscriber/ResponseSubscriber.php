28a1eeee2b7ad1596510826015ff9afac8306597
Mini-T <Minilucker.74@gmail.com>
2023-05-24 21:49:24 +0200

    added operation on post user response in subscriber

--- src/Subscriber/ResponseSubscriber.php
+++ src/Subscriber/ResponseSubscriber.php
@@ -3,16 +3,14 @@
 namespace App\Subscriber;
 
 use ApiPlatform\Symfony\EventListener\EventPriorities;
-use App\Entity\RefreshToken;
 use App\Repository\RefreshTokenRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Gesdinet\JWTRefreshTokenBundle\Doctrine\RefreshTokenRepositoryInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\FinishRequestEvent;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
-class ErasePreviousRefreshToken implements EventSubscriberInterface
+
+class ResponseSubscriber implements EventSubscriberInterface
 {
 
     private $entityManager;
@@ -25,18 +23,23 @@ class ErasePreviousRefreshToken implements EventSubscriberInterface
     public static function getSubscribedEvents(): array
     {
         return [
-            KernelEvents::RESPONSE => ['erasePreviousRefreshToken', EventPriorities::PRE_WRITE]
+            KernelEvents::RESPONSE => ['handleResponse', EventPriorities::PRE_WRITE]
         ];
     }
 
-    public function erasePreviousRefreshToken(ResponseEvent $event)
+    public function handleResponse(ResponseEvent $event)
     {
-        if ($event->getRequest()->getRequestUri() === ('/api/login') && $event->getResponse()->isOk()) {
+        $req = $event->getRequest();
+        $responseIsOk = $event->getResponse()->isOk();
+        if ($req->getRequestUri() === ('/api/login') && $responseIsOk) {
             $email = json_decode($event->getRequest()->getContent(), associative: true)["email"];
             $res = $this->entityManager->findByEmail($email);
             if(count($res) > 1){
             $this->entityManager->remove($res[0], flush: true);
             }
+        }
+        if ($req->getRequestUri() === '/api/users' && $req->isMethod('POST') && $event->getResponse()->getStatusCode() === 201) {
+            $event->setResponse(new JsonResponse(['message' => 'Successfully created user']));
         }
 
     }
